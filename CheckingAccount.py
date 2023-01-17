776b9ca82014bde9c4647a34e39b6bd74298f1c9
ZRhodes94 <ZRhodes94@gmail.com>
2023-01-17 11:31:14 -0600

    Update CheckingAccount.py

--- CheckingAccount.py
+++ CheckingAccount.py
@@ -7,28 +7,32 @@ class CheckingAccount:
 
     def debit(self, amount):
         self.__balance += amount
-        print(f"{amount: .2f} was added to your account balance.\n")
+        print(f"{amount: .2f} was added to your account balance.")
+        self.check_balance()
 
     def credit(self, amount):
         if amount <= self.__balance:
             self.__balance -= amount
-            print(f"{amount: .2f} was removed from your account balance.\n")
+            print(f"{amount: .2f} was removed from your account balance.")
+            self.check_balance()
         
         elif amount >= self.__balance:
-            print("Insufficent funds in account.\n")
+            print(f"Insufficent funds in account number {self.__account_number}.")
+            self.check_balance()
 
     def check_balance(self):
-        print(f"Your account balance is {self.__balance: .2f}\n")
+        print(f"{self.name}, your account balance is {self.__balance: .2f}\n")
 
 def main():
     myAccount = CheckingAccount("Zach", "123 Sesame Street", 123456789, 0.00)
 
-    myAccount.debit(1000)
-    myAccount.debit(150000)
-    myAccount.check_balance()
-    myAccount.credit(150000)
-    myAccount.check_balance()
+    myAccount.debit(1000.00)
+    myAccount.debit(150000.00)
+    myAccount.credit(150000.00)
     myAccount.credit(1050.00)
-    myAccount.check_balance()
+    myAccount.debit(1500.00)
+    myAccount.debit(1000.00)
+    myAccount.credit(1250.00)
+
 
 main()
